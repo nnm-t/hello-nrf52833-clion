18c23c20d64649ae054dea98222610daeafb50d7
Noname Kamisawa <noname@kamisawa.net>
2021-07-02 00:38:46 +0900

    Create main loop for C++

---
+++ cppmain.cpp
@@ -0,0 +1,24 @@
+#include "cppmain.h"
+#include "nrf_delay.h"
+#include "boards.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void)
+{
+    bsp_board_init(LEDS_NUMBER);
+}
+
+void loop(void)
+{
+    // LED0をON/OFF切替
+    bsp_board_led_invert(0);
+    // 50ms待機
+    nrf_delay_ms(50);
+}
+
+#ifdef __cplusplus
+}
+#endif
--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include <stdbool.h>
-#include <stdint.h>
 
-#include "nrf_delay.h"
-#include "boards.h"
+#include "cppmain.h"
 
 int main(void)
 {
-    bsp_board_init(LEDS_NUMBER);
+    setup();
 
     while (true) {
-        // LED0をON/OFF切替
-        bsp_board_led_invert(0);
-        // 50ms待機
-        nrf_delay_ms(50);
+        loop();
     }
 }
